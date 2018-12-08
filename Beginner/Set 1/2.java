bc0117266b2fc38289047888bd4228cea26ea5c2
Yash Kumar Arora <arorayash1998@gmail.com>
2018-12-08 23:54:09 +0530

    Check number is even or odd

---
+++ Beginner/Set 1/2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Test {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    if(n>0) {
+      if(n%2==0)
+        System.out.println("Even");
+      else
+        System.out.println("Odd");
+    }
+    else
+      System.out.println("Invalid");
+  }
+}
