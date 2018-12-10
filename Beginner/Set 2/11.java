f90ef82fe4cf1d94c940d305e1de2724487a882f
Yash Kumar Arora <arorayash1998@gmail.com>
2018-12-10 12:07:02 +0530

    Power of a number

---
+++ Beginner/Set 2/11.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int p = sc.nextInt();
+
+    System.out.println((int)Math.pow(n,p));
+  }
+}
