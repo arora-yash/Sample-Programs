607d45f64e444ad47892281d83e2b5f9db250f39
Yash Kumar Arora <arorayash1998@gmail.com>
2019-01-22 22:52:00 +0530

    Count the number of numeric characters in a given string

---
+++ Beginner/Set 4/35.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+
+    char ch;
+    int count = 0;
+    for(int i = 0;i<str.length();i++) {
+      ch = str.charAt(i);
+      if(ch >= 48 && ch <= 57)
+        count++;
+    }
+    System.out.println(count);
+  }
+}
