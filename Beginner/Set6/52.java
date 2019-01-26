bee7cbbcf539d54bdc281966e9da2f63e859feb9
Yash Kumar Arora <arorayash1998@gmail.com>
2019-01-26 11:02:12 +0530

    Print the number in words from 1-10

---
+++ Beginner/Set6/52.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    String s[] = new String[] {"zero","one","two","three","four","five", "six", "seven", "eight", "nine", "ten"};
+
+    System.out.println(s[n]);
+  }
+}
