131aac33b2c334f0a6470607e2277bed2ce64f1c
Yash Kumar Arora <arorayash1998@gmail.com>
2019-01-27 17:11:27 +0530

    Find the minimum among 10 numbers

---
+++ Beginner/Set7/63.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int a[] = new int[10];
+
+    for(int i = 0;i<10;i++)
+      a[i] = sc.nextInt();
+
+    int min = a[0];
+    for(int i = 1;i<10;i++)
+      if(min>a[i])
+        min = a[i];
+
+    System.out.println(min);
+  }
+}
