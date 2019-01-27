2fad2a0de0ce6b24b0086888e0172f7a25a4bd4d
Yash Kumar Arora <arorayash1998@gmail.com>
2019-01-28 00:36:12 +0530

    Round off the number to its nearest greater integer

---
+++ Beginner/Set8/74.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    float n = sc.nextFloat();
+    System.out.println((int)Math.ceil(n));
+  }
+}
