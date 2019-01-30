60bd81ffc10b3145a0aa947759a74d3f7d503e2f
Yash Kumar Arora <arorayash1998@gmail.com>
2019-01-30 11:58:05 +0530

    Find the area with 5 decimal places

---
+++ Beginner/Set9/82.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    float l = sc.nextFloat();
+    float b = sc.nextFloat();
+
+    System.out.printf("%.5f",l*b);
+  }
+}
