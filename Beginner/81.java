2209a1a75f6dddb3f660559c765ca3e5ab0ebc8b
Yash Kumar Arora <arorayash1998@gmail.com>
2019-02-03 09:48:27 +0530

    Difference between the clan numbers to decide the fight

---
+++ Beginner/81.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int kabali_num = sc.nextInt();
+    int other_num = sc.nextInt();
+
+    System.out.println(Math.abs(kabali_num-other_num));
+  }
+}
