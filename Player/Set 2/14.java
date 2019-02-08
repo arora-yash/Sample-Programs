670168db974bee203723884f402fc78b4cc42718
Yash Kumar Arora <arorayash1998@gmail.com>
2019-02-08 17:57:46 +0530

    Print the given String in reversed order without the vowels

---
+++ Player/Set 2/14.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+
+    char ch;
+    for(int i = str.length()-1;i>=0;i--) {
+      ch = str.charAt(i);
+      if(ch!='a'&&ch!='e'&&ch!='i'&&ch!='o'&&ch!='u'&&ch!='A'&&ch!='E'&&ch!='I'&&ch!='O'&&ch!='U')
+        System.out.print(ch);
+    }
+  }
+}
