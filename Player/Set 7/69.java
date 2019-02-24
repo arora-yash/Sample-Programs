d0cdd7c9a10110112417c2bfeeed12d20aec5fe3
Yash Kumar Arora <arorayash1998@gmail.com>
2019-02-24 11:38:15 +0530

    Print the array after deleting K items

---
+++ Player/Set 7/69.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+
+    int a[] = new int[n];
+
+    for(int i = 0;i<n-k;i++) {
+      a[i] = sc.nextInt();
+      System.out.print(a[i]+" ");
+    }
+  }
+}
