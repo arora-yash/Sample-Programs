49399635e854e0ae16bfa8555b913727a8fb3d14
Yash Kumar Arora <arorayash1998@gmail.com>
2019-03-09 10:14:44 +0530

    Convert binary number to decimal

---
+++ Player/Set 10/92.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String binaryNumber = sc.nextLine();
+    System.out.println(Integer.parseInt(binaryNumber,2));
+  }
+}
